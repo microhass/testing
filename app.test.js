96e0b11bd2f133cba1ca61cf8ddc6ec0726f81d6
hassanShakur <hass.shakurhassan@gmail.com>
2023-06-12 19:51:08 +0300

    Initial commit & with tasks 1 & 2

---
+++ app.test.js
@@ -0,0 +1,24 @@
+const stringLength = (str) => {
+  if (str.length < 1 || str.length > 10) {
+    throw Error('String must be in range 1 - 10');
+  }
+  return str.length;
+};
+
+test('Returns the correct length of string', () => {
+  expect(stringLength('abc')).toBe(3);
+});
+
+test('String has greater than 0 chars', () => {
+  expect(() => stringLength('')).toThrow(/range/);
+});
+
+test('String has at most 10 chars', () => {
+  expect(() => stringLength('0123456789abcd')).toThrow(/range/);
+});
+
+const reverseString = (str) => str.split('').reverse().join('');
+
+test('Function returns the reverse of a string', () => {
+  expect(reverseString('hannan')).toBe('nannah');
+});
